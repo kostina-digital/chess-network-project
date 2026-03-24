b57e2a19f4551ccf28401e0ada70c628aa4641ec
kostina__digital <kostina.digital@gmail.com>
2026-03-24 13:22:08 +0100

    Refactor HeroSection component for improved readability and layout
    - Reformatted JSX structure in `heroSection.tsx` for better readability.
    - Ensured consistent indentation and spacing for maintainability.
    - No functional changes were made, focusing solely on code style improvements.

--- src/components/landingPage/heroSection.tsx
+++ src/components/landingPage/heroSection.tsx
@@ -4,17 +4,29 @@ import SignInBtn from "../buttons/signInBtn";
 import SignUpBtn from "../buttons/signUpBtn";
 
 export default function HeroSection() {
-    return (
-        <div className="flex items-center justify-center gap-8">
-            <div className="flex flex-col items-center justify-between gap-8 mr">
-                <h1 className="h1-style">Connect, Analyze, and Master Chess Together</h1>
-                <h3 className="h3-style">Join a thriving community of chess enthusiasts. Share your game analysis, learn from others, and connect with players around the world.</h3>
-                <div className="flex items-center justify-center gap-6">
-                    <SignInBtn />
-                    <SignUpBtn />
-                </div>
-            </div>
-            <Image src={HeroImg.src} alt="Hero Section" width={600} height={500} className="rounded-lg" />
-        </div >
-    );
+  return (
+    <div className="flex items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-between gap-8 mr">
+        <h1 className="h1-style">
+          Connect, Analyze, and Master Chess Together
+        </h1>
+        <h3 className="h3-style">
+          Join a thriving community of chess enthusiasts. Share your game
+          analysis, learn from others, and connect with players around the
+          world.
+        </h3>
+        <div className="flex items-center justify-center gap-6">
+          <SignInBtn />
+          <SignUpBtn />
+        </div>
+      </div>
+      <Image
+        src={HeroImg.src}
+        alt="Hero Section"
+        width={600}
+        height={500}
+        className="rounded-lg"
+      />
+    </div>
+  );
 }
